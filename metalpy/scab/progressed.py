5b129ea349bd002cbb971dc300fd1a45e4d6040b
yanang007 <yanang007@users.noreply.github.com>
2022-09-25 23:31:32 +0800

    :sparkles: progressed现在支持在其他patch中手动更新进度条
    如果未set_manual_update，则第一次手动更新进度时会重置进度并自动set_manual_update
    警告：如果依赖自动判断可能在手动更新前自动更新仍会进行一次，导致进度条错误地额外更新一次

--- metalpy/scab/progressed.py
+++ metalpy/scab/progressed.py
@@ -6,11 +6,46 @@ from SimPEG.simulation import BaseSimulation
 from .injectors import extends, after
 
 
+@extends(BaseSimulation, 'progressbar')
+@property
+def __BaseSimulation_ext_progressbar(self):
+    if not hasattr(self, '_progressbar'):
+        self._progressbar = None
+    return self._progressbar
+
+
+@extends(BaseSimulation, 'progressbar')
+@__BaseSimulation_ext_progressbar.setter
+def __BaseSimulation_ext_progressbar(self, p):
+    if hasattr(self, '_progressbar'):
+        ret = self._progressbar
+    else:
+        ret = None
+    self._progressbar = p
+    return ret
+
+
 @extends(BaseSimulation, 'progress_on')
 def __BaseSimulation_ext_progress_on(self):
-    progressbar = tqdm.tqdm(total=len(self.survey.receiver_locations))
+    self.progressbar = tqdm.tqdm(total=len(self.survey.receiver_locations))
+    self.manual_update = False  # 指示是否有其它插件在手动更新进度条
 
     @after(self, 'evaluate_integral')
     def wrapper(*args, **kwargs):
-        progressbar.update(1)
+        if not self.manual_update:
+            self.progressbar.update(1)
+
+
+@extends(BaseSimulation, 'set_manual_update')
+def __BaseSimulation_ext_update_progress(self):
+    self.manual_update = True
+
+
+@extends(BaseSimulation, 'update_progress')
+def __BaseSimulation_ext_update_progress(self, count):
+    if self.progressbar is not None:
+        if not self.manual_update:
+            self.progressbar.reset()
+            self.manual_update = True  # 切换为用户更新模式，此前可能已被自动更新过，因此需要重置
+        self.progressbar.update(count)
 
