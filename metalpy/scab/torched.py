64cf1a729b95a52ec57ae7d37b22057a5f8b68a0
yanang007 <yanang007@users.noreply.github.com>
2022-09-26 01:29:42 +0800

    :sparkles: 扩展计算batch size的patch接口

--- metalpy/scab/torched.py
+++ metalpy/scab/torched.py
@@ -12,5 +12,19 @@ use_patch(ext_torch)
 @extends(BaseSimulation, 'torch_on')
 def __BaseSimulation_ext_torch_on(self, device='cpu'):
     self.torch_device = torch.device(device)
+
+    extends(self, 'estimate_memory_cost')(__BaseSimulation_ext_estimate_memory_cost)
+    extends(self, 'estimate_batch_size')(__BaseSimulation_ext_estimate_batch_size)
+
     self.torch_on_impl()
 
+
+def __BaseSimulation_ext_estimate_memory_cost(self, size=None):
+    # size = self.survey.receiver_locations.shape[0] if size is None else size
+    # return 100 * size * self.modelMap.shape[0]
+
+    return None
+
+
+def __BaseSimulation_ext_estimate_batch_size(self, memory):
+    return int(memory / self.estimate_memory_cost(1))
