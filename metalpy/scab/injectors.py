1f8c12b75d56edf4a8d6972a03936331f266d45a
yanang007 <yanang007@users.noreply.github.com>
2022-09-25 12:54:02 +0800

    :bug: 修复进度条提前更新的错误

--- metalpy/scab/injectors.py
+++ metalpy/scab/injectors.py
@@ -39,3 +39,20 @@ class before:
         cmd = f'self.cls.{self.name} = wrapper'
         exec(cmd)
         return wrapper
+
+
+class after:
+    def __init__(self, cls, name):
+        self.cls = cls
+        self.name = name
+
+    def __call__(self, func):
+        orig = self.cls.__getattribute__(self.name)
+        def wrapper(*args, **kwargs):
+            result = orig(*args, **kwargs)
+            func(*args, **kwargs)
+            return result
+
+        cmd = f'self.cls.{self.name} = wrapper'
+        exec(cmd)
+        return wrapper
--- metalpy/scab/progressed.py
+++ metalpy/scab/progressed.py
@@ -3,14 +3,14 @@ import sys
 import tqdm
 from SimPEG.simulation import BaseSimulation
 
-from .injectors import extends, before
+from .injectors import extends, after
 
 
 @extends(BaseSimulation, 'progress_on')
 def __BaseSimulation_ext_progress_on(self):
     progressbar = tqdm.tqdm(total=len(self.survey.receiver_locations))
 
-    @before(self, 'evaluate_integral')
+    @after(self, 'evaluate_integral')
     def wrapper(*args, **kwargs):
         progressbar.update(1)
 
