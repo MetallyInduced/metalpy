790de12440a8ce01d507533dbd84252536872ce0
yanang007 <yanang007@users.noreply.github.com>
2022-11-15 01:06:19 +0800

    :bug: 修复无法识别名字含'-'字符的Worker的问题

--- metalpy/mepa/worker.py
+++ metalpy/mepa/worker.py
@@ -2,15 +2,17 @@ class Worker:
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
-        self.group = name.split('-')[0]
+        segments = name.split('-')
+        # name中最后一个'-'前的内容 或 name，保证name不为空时，结果不为字符串
+        self.group = '-'.join(segments[:-1]) or name
 
-        if self.group == name:
+        if self.group == name or not segments[-1].isdigit():
             print("WARNING: worker's name must follow the format '{group name}-{in-group id}', "
-                  "and in-group id is assumed to start from zero,"
+                  "and in-group id is assumed to start from zero, "
                   "if not, something may go wrong.")
             self.id = None
         else:
-            self.id = int(name.split('-')[1])
+            self.id = int(segments[-1])
 
     def get_name(self):
         return self.name
