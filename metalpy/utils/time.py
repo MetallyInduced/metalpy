46621ce5b2f55727bfef56fb81298b42c8a31ff3
yanang007 <yanang007@users.noreply.github.com>
2022-11-28 00:09:54 +0800

    :sparkles: 实现毫秒级Timer类

--- metalpy/utils/time.py
+++ metalpy/utils/time.py
@@ -8,18 +8,20 @@ class Timer:
         pass
 
     def __enter__(self):
-        self.started = time.time()
+        self.started = time.perf_counter()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stopped = time.time()
+        self.stopped = time.perf_counter()
         self.elapsed = self.stopped - self.started
 
     def __str__(self):
-        elapsed_mins, elapsed_secs = epoch_time(self.started, self.stopped)
-        if elapsed_mins == 0:
-            return f"{elapsed_secs} s"
-        else:
+        elapsed_mins, elapsed_secs, elapsed_milis = perf_epoch_time(self.started, self.stopped)
+        if elapsed_mins > 0:
             return f"{elapsed_mins} min {elapsed_secs} s"
+        elif elapsed_secs > 0:
+            return f"{elapsed_secs} s {elapsed_milis} ms"
+        else:
+            return f"{elapsed_milis} ms"
 
 
 def epoch_time(start_time, end_time):
@@ -27,3 +29,15 @@ def epoch_time(start_time, end_time):
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+
+def perf_epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_milis = elapsed_time / 1000
+    elapsed_secs = int(elapsed_milis / 1000)
+    elapsed_mins = int(elapsed_secs / 60)
+
+    elapsed_milis -= elapsed_secs * 1000
+    elapsed_secs -= elapsed_mins * 60
+
+    return elapsed_mins, elapsed_secs, elapsed_milis
