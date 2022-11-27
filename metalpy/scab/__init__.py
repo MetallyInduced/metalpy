dac3326f8573b5c42cdf552e38f335ea70125314
yanang007 <yanang007@users.noreply.github.com>
2022-11-28 00:09:23 +0800

    :children_crossing: 消除模块对torch的依赖，只在有需要时引入

--- metalpy/scab/__init__.py
+++ metalpy/scab/__init__.py
@@ -1,4 +1,8 @@
 from .distributed import Distributed
 from .progressed import Progressed
-from .torched import Torched
 from .simpeg_patch_context import simpeg_patched
+
+try:
+    from .torched import Torched
+except ImportError:
+    pass  # most likely torch is not installed
