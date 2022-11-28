5ca2b649ab7dfdccf3640232861a9f4e4a1e1136
yanang007 <yanang007@users.noreply.github.com>
2022-11-28 13:57:58 +0800

    :sparkles: 实现ti_init_once缓式初始化

--- metalpy/utils/taichi.py
+++ metalpy/utils/taichi.py
@@ -1,14 +1,40 @@
 import taichi as ti
 
-ti.init(arch=ti.cpu)
+from metalpy.mepa import LazyEvaluator
+
+ti_args = {
+    'arch': ti.cpu
+}
+ti_inited = False
+
+
+def ti_prepare(**kwargs):
+    ti_args.update(kwargs)
+
+
+def ti_init_once():
+    if not ti_inited:
+        ti.init(**ti_args)
 
 
 def ti_func(fn, is_real_function=None):
     if is_real_function is None:
-        return ti.func(fn)
+        evaluator = LazyEvaluator(ti.func, fn)
     else:
-        return ti.func(fn, is_real_function)
+        evaluator = LazyEvaluator(ti.func, fn, is_real_function)
+
+    def lazy_evaluator_wrapper(*args, **kwargs):
+        ti_init_once()
+        return evaluator.get()(*args, **kwargs)
+
+    return lazy_evaluator_wrapper
 
 
 def ti_kernel(fn):
-    return ti.kernel(fn)
+    evaluator = LazyEvaluator(ti.kernel, fn)
+
+    def lazy_evaluator_wrapper(*args, **kwargs):
+        ti_init_once()
+        return evaluator.get()(*args, **kwargs)
+
+    return lazy_evaluator_wrapper
