a415769028d7bb0407cf14229c1b0aae4d369279
yanang007 <yanang007@users.noreply.github.com>
2022-11-28 17:15:57 +0800

    :bug: 修复Timer单位错误的问题

--- metalpy/utils/time.py
+++ metalpy/utils/time.py
@@ -8,20 +8,23 @@ class Timer:
         pass
 
     def __enter__(self):
-        self.started = time.perf_counter()
+        self.started = time.perf_counter_ns()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stopped = time.perf_counter()
+        self.stopped = time.perf_counter_ns()
         self.elapsed = self.stopped - self.started
 
     def __str__(self):
-        elapsed_mins, elapsed_secs, elapsed_milis = perf_epoch_time(self.started, self.stopped)
+        elapsed_mins, elapsed_secs, elapsed_milis, elapsed_micros = \
+            perf_ns_epoch_time(self.started, self.stopped)
         if elapsed_mins > 0:
             return f"{elapsed_mins} min {elapsed_secs} s"
         elif elapsed_secs > 0:
             return f"{elapsed_secs} s {elapsed_milis} ms"
+        elif elapsed_milis > 0:
+            return f"{elapsed_milis + elapsed_micros / 1000:.5f} ms"
         else:
-            return f"{elapsed_milis} ms"
+            return f"{elapsed_micros:.5f} us"
 
 
 def epoch_time(start_time, end_time):
@@ -31,13 +34,15 @@ def epoch_time(start_time, end_time):
     return elapsed_mins, elapsed_secs
 
 
-def perf_epoch_time(start_time, end_time):
+def perf_ns_epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time
-    elapsed_milis = elapsed_time / 1000
+    elapsed_micros = elapsed_time / 1000
+    elapsed_milis = int(elapsed_micros / 1000)
     elapsed_secs = int(elapsed_milis / 1000)
     elapsed_mins = int(elapsed_secs / 60)
 
+    elapsed_micros -= elapsed_milis * 1000
     elapsed_milis -= elapsed_secs * 1000
     elapsed_secs -= elapsed_mins * 60
 
-    return elapsed_mins, elapsed_secs, elapsed_milis
+    return elapsed_mins, elapsed_secs, elapsed_milis, elapsed_micros
