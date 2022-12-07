565418dc30887f67fa344f5df6cad6c57db70c11
yanang007 <yanang007@users.noreply.github.com>
2022-12-07 14:39:12 +0800

    :technologist: 简化magnetics.Survey的定义方式

--- metalpy/scab/utils/misc.py
+++ metalpy/scab/utils/misc.py
@@ -1,3 +1,6 @@
+from typing import Union, Iterable
+
+import numpy as np
 from SimPEG.utils import mat_utils
 
 
@@ -39,10 +42,58 @@ class Field:
 
 
 def define_inducing_field(strength, inclination, declination) -> Field:
-    """
-    :param strength: 场强
-    :param inclination: 磁倾角
-    :param declination: 磁偏角
-    :return:
+    """定义一个外部激发场
+
+    Parameters
+    ----------
+    strength
+        场强
+    inclination
+        场倾角
+    declination
+        场偏角
+
+    Returns
+    -------
+        定义的物理场
     """
     return Field(strength, inclination, declination)
+
+
+def define_magnetics_survey(receiver_points: np.ndarray,
+                            components: Union[str, Iterable[str]],
+                            *other_receivers: Iterable[Union[np.ndarray, Union[str, Iterable[str]]]],
+                            inducing_field: Field = None):
+    """定义一个磁力学的Survey
+
+    Parameters
+    ----------
+    receiver_points
+        接收点
+    components
+        接收点所需要计算的分量
+    other_receivers
+        其他接受点和对应分量，可以有多个，按 rp1, comp1, rp2, comp2, ... 的顺序传入
+    inducing_field
+        外部激发场，如果为None则使用默认值 (50000nT, 90°, 0°)
+
+    Returns
+    -------
+        包含接收点和外部场信息的磁力学Survey
+    """
+    from SimPEG.potential_fields import magnetics
+
+    receiver_list = magnetics.receivers.Point(receiver_points, components=components)
+    receiver_list = [receiver_list]
+
+    assert len(other_receivers) % 2 == 0, '*other_receivers* must be paris of receiver_points, components, ' \
+                                          'etc. define_magnetics_survey(rp1, comp1, rp2, comp2, rp3, comp3)'
+    for receiver_points, components in zip(other_receivers[::2], other_receivers[1::2]):
+        receiver_list.append(magnetics.receivers.Point(receiver_points, components=components))
+
+    source_field = magnetics.sources.SourceField(
+        receiver_list=receiver_list, parameters=inducing_field
+    )
+    survey = magnetics.survey.Survey(source_field)
+
+    return survey
