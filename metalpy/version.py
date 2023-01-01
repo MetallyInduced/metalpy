21b9ec913207ab878c3ee0a9b21c9f85788a0cf2
yanang007 <yanang007@users.noreply.github.com>
2023-01-02 00:43:32 +0800

    :sparkles: version.py添加post_distance，指示距上一个tag的commit数

--- metalpy/version.py
+++ metalpy/version.py
@@ -5,7 +5,16 @@ from ._version import get_version, get_version_tuple
 __version__ = get_version()
 version_tuple = get_version_tuple()
 full_version = __version__
-short_version = NamedTuple('ShortVersion', major=str, minor=str, patch=str)(*version_tuple[:3])
+base_version = NamedTuple('ShortVersion', major=int, minor=int, patch=int)(*version_tuple[:3])
+short_version = '.'.join((str(i) for i in version_tuple[:3]))
 is_release = len(version_tuple) == 3
 
-del get_version, get_version_tuple
+post_distance = 0
+revision_prefix = 'post'
+if not is_release:
+    for ver_desc in version_tuple[3:]:
+        if ver_desc.startswith(revision_prefix):
+            post_distance = int(version_tuple[3][len(revision_prefix):])
+            break
+
+del get_version, get_version_tuple, revision_prefix
